9980661e6a372c5e5c91ba17cb93581d5eff7f88
thomhie0416 <48708193+thomhie0416@users.noreply.github.com>
2020-01-23 19:21:12 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>lOGIN</title>
+</head>
+<body>
+<a href="uploadtoserver.php">CLICK ME FOR REAL</a>
+</body>
+</html>
---
+++ uploadtoserver.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<form action="upload.php" method="post" enctype="multipart/form-data">
+<label>Select image to upload</label>
+<input type="file" name="file_upload" />
+<input type="submit" name="upload"/>
+</form>
+
+</body>
+</html>
